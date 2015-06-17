a4927f9d6dc1dcb389eddd829e082e46d17941b8
htrammel <harold.trammel@aspca.org>
2015-06-16 22:32:26 -0500

    v1r2
    still exploring the data

--- scratch_pad.R
+++ scratch_pad.R
@@ -1,3 +1,8 @@
+require(dplyr)
+require(ggplot2)
+require(lubridate)
+
+
 if(!file.exists("./data")) {dir.create("./data")}
 dataFile <- "repdata-data-StormData.csv.bz2"
 dataRDS <- "./data/storm.rds"
@@ -6,6 +11,8 @@ dataRDS <- "./data/storm.rds"
 if (!file.exists(dataFile)) {
 	    download.file("https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2", 
 		    dataFile, method = "curl", mode = "wb")
+        storm_df <- read.csv(bzfile(dataFile))
+        saveRDS(storm_df, dataRDS)
     } else if (!file.exists(dataRDS)) {
         storm_df <- read.csv(bzfile(dataFile))
         saveRDS(storm_df, dataRDS)
@@ -16,3 +23,7 @@ if (exists("storm_df") == FALSE) {
     storm_df <- readRDS(dataRDS)
 }
 
+storm_df$BGN_DATE <- dmy(as.character(storm_df$BGN_DATE))
+
+
+s_df <- storm_df %>% group_by(year(BGN_DATE), EVTYPE ) %>% summarise(mean(FATALITIES))
