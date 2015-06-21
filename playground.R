ef2568954698b402687e299d39f996a25448d3c7
htrammel <harold.trammel@aspca.org>
2015-06-21 16:59:39 -0500

    Final
    I am not happy with the finished product but it is turned in,

--- playground.R
+++ playground.R
@@ -1,14 +1,57 @@
 # remove summary
 require(plyr)
 require(dplyr)
-require(stringr)
-require(qdap)
+require(ggplot2)
+require(RColorBrewer)
 
-animals <- c("Dog","Cat","Catfish","Dogfish"
-             ,"Mountain Dog","Wildcat","Hound dog"
-             ,"Dirty Dog","Cattle Dog", "Dog/Mixed Breed"
-             ,"Dog/ Purebred", "Calico Cat", "Cat Stray")
+ev_df <- df %>% 
+    filter(EVTYPE %in% tevt) %>%
+    group_by(year(BGN_DATE), EVTYPE) %>%
+    summarise(num_evt = n()) %>% 
+    arrange(desc(num_evt)) %>%
+    top_n(10, num_evt)  
 
-animals <- gsub("Cat","Feline", animals)
+names(ev_df) <- c("year", "eventType", "num_evt")
+hp0 <- ggplot(ev_df, aes(eventType, num_evt, fill = year)) +
+    geom_bar(stat = 'identity', position = 'stack') + 
+    coord_flip() +
+    scale_fill_continuous(name = "Year") +
+    labs (title = "Most Frequent Storm Events"
+          , x = "Storm Event Type"
+          , y = "Number of Events")
+print(hp0)
 
-print(animals)
+th_df <- df %>% 
+    filter(EVTYPE %in% tevt) %>%
+    group_by(year(BGN_DATE), EVTYPE) %>%
+    summarise(sum_effect = sum(humEffect)) %>% 
+    arrange(desc(sum_effect)) %>%
+    top_n(10, sum_effect)  
+
+names(th_df) <- c("year", "eventType", "sum_effect")
+hp1 <- ggplot(th_df, aes(eventType, sum_effect, fill = year)) +
+    geom_bar(stat = 'identity', position = 'stack') + 
+    coord_flip() +
+    scale_fill_continuous(name = "Year") +
+    labs (title = "Human Effects (Fatalities + Injuries) from Storms"
+          , x = "Storm Event Type"
+          , y = "Number of Fatalities and Injuries")
+print(hp1)
+
+
+td_df <- df %>% 
+    filter(EVTYPE %in% tevt) %>%
+    group_by(year(BGN_DATE),EVTYPE) %>%
+    summarise(sum_damage = sum(damage)) %>% 
+    arrange(desc(sum_damage)) %>%
+    top_n(10, sum_damage)
+names(td_df) <- c("year", "eventType", "sum_damage")
+
+hp2 <- ggplot(td_df, aes(eventType, sum_damage/1000000, fill = year)) +
+    geom_bar(stat = 'identity', position = 'stack') + 
+    coord_flip() +
+    scale_fill_continuous(name = "Year") +
+    labs (title = "Storm Damage (Property + Crop)"
+          , x = "Storm Event Type"
+          , y = "Damage in Dollars (Millions)")
+print(hp2)
--- regex-clean-data.R
+++ regex-clean-data.R
@@ -16,7 +16,7 @@ if (!file.exists(dataFile)) {
         # needed for my Windows PC
         download.file(fileURL, destfile=dataFile)
     } else {
-        # needed for my Mac
+        # ne landeded for my Mac
         download.file(fileURL, destfile=dataFile, method = "curl", mode = "wb")
     }
     storm_df <- read.csv(bzfile(dataFile), stringsAsFactors = FALSE)
@@ -31,89 +31,84 @@ if (exists("storm_df") == FALSE) {
     storm_df <- readRDS(dataRDS)
 }
 
+# select only the needed columns
 df <- select(storm_df, one_of(c("BGN_DATE", "EVTYPE", "FATALITIES","INJURIES",
-"PROPDMG", "PROPDMGEXP", "CROPDMG", "CROPDMGEXP")))
-
+                                "PROPDMG", "PROPDMGEXP", "CROPDMG", "CROPDMGEXP")))
+# function to convert unit abbreviations to numeric values 
+damageUnit <- function (cv) {
+    for (i in cv) {
+        if (i == "B") { return (1000000000) }
+        else if (i == "M") { return (1000000) }
+        else if (i == "K") { return (1000) }
+        else return (1)
+    }
+}
 # general conversions to make things better
 df$EVTYPE <- str_to_title(as.character(df$EVTYPE))
 df$BGN_DATE <- as.character.Date(df$BGN_DATE)
 df$BGN_DATE <- dmy_hms(df$BGN_DATE)
-df <- mutate(df, EVT_YR = year(df$BGN_DATE))
-
-df <- filter(df, EVT_YR >= 1994 & EVT_YR <= 2014 )
+# filter for only past twenty years data
+df <- filter (df, year(df$BGN_DATE) >= 1994 & year(df$BGN_DATE) <= 2014 )
+# filter for only rows with human and economic impact
 df <- filter(df, FATALITIES > 0 | INJURIES > 0 | PROPDMG > 0 | CROPDMG > 0)
+# add new column to hold calculated values
+df <- mutate (df, humEffect = FATALITIES + INJURIES)
+df <- mutate (df, damage = (PROPDMG * damageUnit(PROPDMGEXP)) +
+                  (CROPDMG * damageUnit(CROPDMGEXP)))
 
 # remove leading and laging spaces
 df$EVTYPE <- Trim(df$EVTYPE)
-# Avalance
-df$EVTYPE <- gsub("([ a-zA-Z/]*)*Avalanche", "Avalanche", df$EVTYPE)
+
+# MISCELLANOUS ##########################################
+# Debris Flow
+df$EVTYPE <- gsub("[ a-zA-Z/]*[s]*[/]*slide", "Debris Flow", df$EVTYPE, ignore.case = T)
+# Dense Fog 
+df$EVTYPE <- gsub("[ a-zA-Z/]*[s]*[/]*Dense Fog", "Dense Fog", df$EVTYPE)
+# Dense Smoke [NO CHANGE]
+
+# WIND AND RAIN RELATED ######################################
 # Wind
 df$EVTYPE <- gsub("W[i]*n[s|d][s]*", "Wind", df$EVTYPE)
-df$EVTYPE <- gsub("Wind[ a-zA-Z/\\.]*", "Wind", df$EVTYPE)
-# blizzard
-df$EVTYPE <- gsub("Blizzard[-/a-zA-Z ]*", "Blizzard", df$EVTYPE)
-df$EVTYPE <- gsub("[/a-zA-Z ]*Blizzard", "Blizzard", df$EVTYPE)
+df$EVTYPE <- gsub("Wind[ a-zA-Z/0-9\\(\\)\\.]*", "Wind", df$EVTYPE)
+# High Wind
+df$EVTYPE <- mgsub(c("Gradient Wind","Extreme Wind"
+                    ,"Storm Force Wind", "Strong Wind"
+                    ,"Gusty Wind")
+                  ,c("High Wind"), df$EVTYPE)
+# Thunderstorm Wind
+df$EVTYPE <- gsub("Tstm", "Thunderstorm", df$EVTYPE)
+df$EVTYPE <- gsub("T[h]*u[n]*[d]*e[e]*r[s]*t[or|ro]*m[s|w]*", "Thunderstorm", df$EVTYPE)
+df$EVTYPE <- gsub("[(]*[a-zA-Z]*[0-9]+([ ]*Mph)*[)]*", " ", df$EVTYPE)
+df$EVTYPE <- gsub("Thunderstorm Wind[\\. a-zA-Z/]*", "Thunderstorm Wind", df$EVTYPE)
+# Hail
+df$EVTYPE <- gsub("Hail([ ]*[0-9]*|[ ]*/[a-zA-Z]*)", "Hail", df$EVTYPE)
+df$EVTYPE <- gsub("[a-zA-Z /]*Hail", "Hail", df$EVTYPE)
+# Lightning
+df$EVTYPE <- gsub("Lightning[a-zA-Z/ \\.]*", "Lightning", df$EVTYPE)
+df$EVTYPE <- gsub("[a-zA-Z/ ]*Lightning", "Lightning", df$EVTYPE)
+# Heavy Rain
+df$EVTYPE <- gsub("[a-zA-Z ]*Rain/Winter Weather", "WW", df$EVTYPE)
+df$EVTYPE <- gsub("Heavy Rain[/a-zA-Z ]*)*", "Heavy Rain", df$EVTYPE)
+df$EVTYPE <- mgsub(c("Excessive Rainfall", "Unseasonal Rain")
+                 , c("Heavy Rain"), df$EVTYPE)
+
+# HEAT RELATED ################################################
+# Drought [NO CHANGE]
+df$EVTYPE <- gsub("Drought[-/a-zA-Z ]*", "Drought", df$EVTYPE)
+df$EVTYPE <- gsub("[/a-zA-Z ]*Drought", "Drought", df$EVTYPE)
+# Dust Devil
+df$EVTYPE <- gsub("Dust Devil Waterspout","Dust Devil", df$EVTYPE)
+# Dust Storm [NO CHANGE]
+# Excessive Heat 
+df$EVTYPE <- gsub("[ a-zA-Z/]* Heat", "Excessive Heat", df$EVTYPE)
+# Heat
+df$EVTYPE <- gsub("Heat [a-zA-Z/ ]+", "Heat", df$EVTYPE)
+
+# FLOOD RELATED ###############################################
 # Coastal Flooding
 df$EVTYPE <- gsub("(Coastal|Cstl|Tidal) Flood(ing)*", "CFLD", df$EVTYPE)
 df$EVTYPE <- gsub("[ a-zA-Z/]*[s]*[/]*Coastal Flood", "CFLD", df$EVTYPE)
 df$EVTYPE <- gsub("Coastal Flood[/]*[a-zA-A /]+", "CFLD", df$EVTYPE)
-# debris flow
-df$EVTYPE <- gsub("[ a-zA-Z/]*[s]*[/]*slide", "Debris Flow", df$EVTYPE, ignore.case = T)
-# dense fog 
-df$EVTYPE <- gsub("[ a-zA-Z/]*[s]*[/]*Dense Fog", "Dense Fog", df$EVTYPE)
-# dense smoke [NO CHANGE]
-
-# # Drought 
-# drou <- c("Excessive Heat/Drought", "Drought/Excessive Heat"
-#           , "Heat Drought", "Heat Wave Drought", "Heat/Drought" 
-#           , "Dry Conditions", "Excessively Dry", "Dry Pattern"
-#           , "Dry Spell", "Dry Weather", "Dryness", "Abnormally Dry")
-# #, "Dry")
-# df$EVTYPE <- mgsub(drou, c("Drought"), df$EVTYPE)
-# 
-# # dust devil
-# df$EVTYPE <- mgsub(c("Dust Devel", "Dust Devil Waterspout"), c("Dust Devil"), df$EVTYPE)
-# 
-# # dust storm 
-# df$EVTYPE <- mgsub(c("Dust Storm/High Winds", "High Winds Dust Storm")
-#                    ,c("Dust Storm"), df$EVTYPE)
-# 
-
-# 
-# # excessive heat 
-# xheat<- c("Heat Wave", "Heat Waves", "Heatburst"
-#           , "Record Heat", "Record High", "Record High Temperature"
-#           , "Record High Temperatures", "Record/Excessive Heat", "Record Warmth")
-# #, "Heat")
-# df$EVTYPE <- mgsub(xheat, c("Excessive Heat"), df$EVTYPE)
-
-# Extreme Wind Chill 
-df$EVTYPE <- gsub("(Bitter|Record|Extended|Extreme) Cold[-/a-zA-Z ]*", "ECWC", df$EVTYPE)
-df$EVTYPE <- gsub("[/a-zA-Z ]*Extreme Cold", "ECWC", df$EVTYPE)
-df$EVTYPE <- gsub("[a-zA-Z ]*(Bitter|Extreme) Wind[ ]*[C|c]?h[a-zA-Z ]+", "ECWC", df$EVTYPE)
-# # Cold/Wind Chill
-df$EVTYPE <- gsub("Cold/Wind Chill", "CWC", df$EVTYPE)
-df$EVTYPE <- gsub("Un[a-zA-Z]+ Cold", "CWC", df$EVTYPE)
-df$EVTYPE <- gsub("Cold (Temp[a-z]+[s]*|Wave|Weather)+", "CWC", df$EVTYPE)
-df$EVTYPE <- gsub("Fog And Cold", "CWC", df$EVTYPE)  ## <=== doesnt work
-df$EVTYPE <- gsub("Cold And[a-zA-Z /]+", "CWC", df$EVTYPE)
-df$EVTYPE <- mgsub(c("High Wind/Cold","Cold/Wind"), c("CWC"), df$EVTYPE)
-df$EVTYPE <- gsub("Cold","CWC", df$EVTYPE)
-### Clean up the previous two
-df$EVTYPE <- mgsub(c("CWC","ECWC")
-    , c("Cold/Wind Chill","Extreme Cold/Wind Chill"), df$EVTYPE)
-
-# # Winter Storm 
-# wstrm <- c()
-# df$EVTYPE <- mgsub(wstrm, c("Winter Storm"), df$EVTYPE)
-# 
-# # Winter Event 
-# wevt<- c("Black Ice", "Accumulated Snowfall","Snow" 
-# , "Blowing Snow" "Cold And Frost" , "Cold And Snow" 
-# )
-# df$EVTYPE <- mgsub(wevt, c("Winter Event"), df$EVTYPE)
-
-
 # Flash flood
 df$EVTYPE <- mgsub(c("Flash Flood/Flood","Flood/Flash Flood"), "Flood", df$EVTYPE)
 df$EVTYPE <- gsub("Flash (Fl[o]+d)(s|ing)*", "FF", df$EVTYPE)
@@ -124,46 +119,81 @@ df$EVTYPE <- gsub("Flood(s|in[g]*)*", "Flood", df$EVTYPE)
 df$EVTYPE <- gsub("Flood/*[ a-zA-Z/(&]*", "Flood", df$EVTYPE)
 df$EVTYPE <- gsub("Thunder[a-zA-Z/ ]+ Flood", "Flood", df$EVTYPE)
 df$EVTYPE <- gsub("[a-zA-Z/ ]+Flood", "Flood", df$EVTYPE)
-### Clean up the first two
+#---------------
 df$EVTYPE <- mgsub(c("CFLD","FF")
                    ,c("Coastal Flood","Flash Flood"), df$EVTYPE)
-# ^--- needs more clean up
+###############################################################
 
+# #### WINTER #############################################
+# Avalance
+df$EVTYPE <- gsub("([ a-zA-Z/]*)*Avalanche", "Avalanche", df$EVTYPE)
+# Blizzard
+df$EVTYPE <- gsub("Blizzard[-/a-zA-Z ]*", "Blizzard", df$EVTYPE)
+df$EVTYPE <- gsub("[/a-zA-Z ]*Blizzard", "Blizzard", df$EVTYPE)
+# Cold/Wind Chill
+df$EVTYPE <- mgsub(c("Fog And Cold","Cold/Wind Chill"
+                     ,"High Wind/Cold","Cold/Wind")
+                   , c("CWC"), df$EVTYPE)
+df$EVTYPE <- gsub("Un[a-zA-Z]+ Cold", "CWC", df$EVTYPE)
+df$EVTYPE <- gsub("Cold (Temp[a-z]+[s]*|Wave|Weather)+", "CWC", df$EVTYPE)
+df$EVTYPE <- gsub("Cold And[a-zA-Z /]+", "CWC", df$EVTYPE)
+df$EVTYPE <- gsub("Cold","CWC", df$EVTYPE)
+# Extreme Wind Chill <================
+df$EVTYPE <- gsub("Ext[a-z]* Cold/Wind Chill", "ECWC", df$EVTYPE)
+df$EVTYPE <- gsub("Record Cold/Wind Chill", "ECWC", df$EVTYPE)
+df$EVTYPE <- gsub("[/a-zA-Z ]*Extreme Cold", "ECWC", df$EVTYPE)
+df$EVTYPE <- gsub("[a-zA-Z ]*(Bitter|Extreme) Wind[ ]*[C|c]?h[a-zA-Z ]+", "ECWC", df$EVTYPE)
+#---------------
+df$EVTYPE <- mgsub(c("CWC","ECWC")
+                   , c("Cold/Wind Chill","Extreme Cold/Wind Chill"), df$EVTYPE)
 # Freezing fog 
-df$EVTYPE <- gsub("[ a-zA-Z/]*[s]*[/]*Freezing Fog", "Freezing Fog", df$EVTYPE)
-# Funnel cloud
-df$EVTYPE <- gsub("Funnel Cloud[a-zA-Z/ ]*", "Funnel Cloud", df$EVTYPE)
-df$EVTYPE <- gsub("[a-zA-Z/ ]*Funnel Cloud", "Funnel Cloud", df$EVTYPE)
-# Hail
-df$EVTYPE <- gsub("Hail([ ]*[0-9]*|[ ]*/[a-zA-Z]*)", "Hail", df$EVTYPE)
-df$EVTYPE <- gsub("[a-zA-Z /]*Hail", "Hail", df$EVTYPE)
+df$EVTYPE <- gsub("[ a-zA-Z/]*[s]*[/]*Freezing Fog", "FZFG", df$EVTYPE)
+# Freeze
+df$EVTYPE <- gsub("Frost/Freeze","FRZ", df$EVTYPE)
+df$EVTYPE <- gsub("(Agricultural|Damaging)*[ ]*Freeze","FRZ", df$EVTYPE)
 # Heavy Snow
-df$EVTYPE <- gsub("Heavy Snow[-/a-zA-Z ]*", "Heavy Snow", df$EVTYPE)
-df$EVTYPE <- gsub("[/a-zA-Z ]*Heavy Snow", "Heavy Snow", df$EVTYPE)
+df$EVTYPE <- gsub("Heavy Snow[-/a-zA-Z ]*", "HSNO", df$EVTYPE)
+df$EVTYPE <- gsub("Snow/Heavy Snow", "HSNO", df$EVTYPE)
+# Winter Storm
+df$EVTYPE <- gsub("Snow[/]*[ ]*Freezing Rain", "WSTRM", df$EVTYPE)
+df$EVTYPE <- gsub("Freezing Rain[/]*[ ]*Snow", "WSTRM", df$EVTYPE)
+df$EVTYPE <- gsub("Mixed [ a-zA-Z]+", "WSTRM", df$EVTYPE)
+df$EVTYPE <- gsub("[a-zA-Z/ ]*Mix", "WSTRM", df$EVTYPE)
+df$EVTYPE <- mgsub(c("Snow/Sleet/Freezing Rain"
+                    ,"Snow And Ice","Snow/Ice Storm"
+                    ,"Falling Snow/Ice","Snow/Sleet"
+                    ,"Snow/ Bitter Cold/Wind Chill"
+                    ,"([ a-zA-Z]*[ ]*)*Rain/Snow","Snow/ Ice")
+                    , c("WSTRM"), df$EVTYPE)
+# Winter Weather
+df$EVTYPE <- gsub("Light Freezing Rain","WW", df$EVTYPE)
+df$EVTYPE <- gsub("Freezing [a-zA-Z/ ]","WW", df$EVTYPE)
+df$EVTYPE <- gsub("Snow[/a-zA-Z ]*","WW", df$EVTYPE)
+#-----------------
+df$EVTYPE <- gsub("FRZ", "Frost/Freeze", df$EVTYPE)
+df$EVTYPE <- gsub("FZFG", "Freezing Fog", df$EVTYPE)
+df$EVTYPE <- gsub("WW", "Winter Weather", df$EVTYPE)
+df$EVTYPE <- gsub("HSNO", "Heavy Snow", df$EVTYPE)
+df$EVTYPE <- gsub("WSTRM", "Winter Storm", df$EVTYPE)
+###############################################################
 
+# HERE WE GO ROUND IN CIRCLES
 # Hurricane/Typhoon
 df$EVTYPE <- gsub("Hurricane[a-zA-Z/ ]*", "HTPH", df$EVTYPE)
 df$EVTYPE <- gsub("Storm Surge[a-zA-Z/ ]*", "HTPH", df$EVTYPE)
 df$EVTYPE <- gsub("HTPH", "Hurricane/Typhoon", df$EVTYPE)
-
-# Lightning
-df$EVTYPE <- gsub("Lightning[a-zA-Z/ \\.]*", "Lightning", df$EVTYPE)
-df$EVTYPE <- gsub("[a-zA-Z/ ]*Lightning", "Lightning", df$EVTYPE)
-
-
-
-# Thunderstorm wind
-df$EVTYPE <- gsub("Tstm", "Thunderstorm", df$EVTYPE)
-df$EVTYPE <- gsub("Thun[d]*e[e]*r[s]*t[or|ro]*m[s|w]*", "Thunderstorm", df$EVTYPE)
-df$EVTYPE <- gsub("[(]*[a-zA-Z]*[0-9]+([ ]*Mph)*[)]*", " ", df$EVTYPE)
-df$EVTYPE <- gsub("Thunderstorm Wind[\\. a-zA-Z/]*", "Thunderstorm Wind", df$EVTYPE)
-
-# tornado
+# Funnel cloud
+df$EVTYPE <- gsub("Funnel Cloud[a-zA-Z/ ]*", "Funnel Cloud", df$EVTYPE)
+df$EVTYPE <- gsub("[a-zA-Z/ ]*Funnel Cloud", "Funnel Cloud", df$EVTYPE)
+# Tornado
 df$EVTYPE <- gsub("Tornado[es]*( F[0-9])*", "Tornado", df$EVTYPE)
-
+# Tropical Storm
+df$EVTYPE <- gsub("Tropical Storm[ a-zA-Z]*", "Tropical Storm", df$EVTYPE)
 
 df$EVTYPE <- Trim(df$EVTYPE)
-
+ot <- storm_df %>% group_by(EVTYPE) %>% count(EVTYPE) %>% arrange(desc(n))
+print(ot)
+print("-----------------")
 t <- df %>% group_by(EVTYPE) %>% count(EVTYPE) %>% arrange(desc(n))
 print(t)
 
